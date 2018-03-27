52ea48b7f689e0b7ad796ac8f78d17890b55848d
Gerd Rausch <gerd.rausch@oracle.com>
2018-03-27 16:14:10 -0700

    libmlx5: Add Shared PD support
    Orabug: 28036829
    
    This is a copy&paste version of the Shared PD support done in libmlx4.
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: Shamir Rabinovitch <shamir.rabinovitch@oracle.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
    
    Orabug: 29356186
    
    Rebase from RDMA Core 17.1 -> 18.2 -> 19.2.
    
    (cherry picked from commit 4795b0142a1827bfd76e825343740e861cf23de7)
    (cherry picked from commit 95352daa2fb7b1faa6ba61819d5b056e07ce993a)
    (cherry picked from commit 854e3206c0669aa0a28c193692b7bd0bd20a8d32)
    cherry-pick-repo=linux-git/RDMA/rdma-core.git
    
    The three commits above were squashed. The first commit adds Shared PD for
    the mlx5 verbs provider. The second commit disables an mlx5 driver
    optimization, "MLX5_INLINE_SCATTER_{32,64}", that is incompatible with
    Shared PD (bug 27949058). The third commit adds "#ifndef
    WITHOUT_ORACLE_EXTENSIONS" markers around Oracle extensions and the
    relevant hunks for mlx5 Shared PD were incorporated here.
    
    Conflicts (17.1 -> 18.2):
            providers/mlx5/mlx5-abi.h
    
    Upstream introduced a new macro, DECLARE_DRV_CMD. This macro is used to
    define mlx5_share_pd().
    
    Conflicts (18.2 -> 19.2):
            kernel-headers/rdma/mlx5-abi.h
    
    Changes made upstream required us to again accept the addition of the
    mlx5_share_pd() macro.
    
    Signed-off-by: Mark Haywood <mark.haywood@oracle.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>

--- providers/mlx5/mlx5.h
+++ providers/mlx5/mlx5.h
@@ -332,6 +332,9 @@ struct mlx5_pd {
 	uint32_t			pdn;
 	atomic_int			refcount;
 	struct mlx5_pd			*mprotection_domain;
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+       uint8_t				is_inherited; /* e.g. via ibv_share_pd */
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 };
 
 struct mlx5_parent_domain {
@@ -734,6 +737,11 @@ int mlx5_query_port(struct ibv_context *context, uint8_t port,
 
 struct ibv_pd *mlx5_alloc_pd(struct ibv_context *context);
 int mlx5_free_pd(struct ibv_pd *pd);
+
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+struct ibv_shpd *mlx5_alloc_shpd(struct ibv_pd *pd, uint64_t share_key, struct ibv_shpd *shpd);
+struct ibv_pd *mlx5_share_pd(struct ibv_context *context, struct ibv_shpd *shpd, uint64_t share_key);
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 
 struct ibv_mr *mlx5_reg_mr(struct ibv_pd *pd, void *addr,
 			   size_t length, int access);
