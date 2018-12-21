LaudateCorpus1/rdma-core

commit 0bb394b0472fa5442225ffcca580c469e072d2e4

libmlx4: Add user level FMR support
Orabug: 21525110

Signed-off-by: Mukesh Kacker <mukesh.kacker@oracle.com>

Orabug: 27594079

(cherry picked from commit 3b1ccb130b88c7920b8c4cec62534be7fefd48ef)
cherry-pick-repo=ofed-libmlx4.git

Signed-off-by: Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
Reviewed-by: Shamir Rabinovitch <shamir.rabinovitch@oracle.com>
Signed-off-by: Aron Silverton <aron.silverton@oracle.com>

providers/mlx4/mlx4.h
@@ -334,9 +334,13 @@ int mlx4_close_xrcd(struct ibv_xrcd *xrcd);
 
 struct ibv_mr *mlx4_reg_mr(struct ibv_pd *pd, void *addr,
 			    size_t length, int access);
+struct ibv_mr *mlx4_reg_mr_relaxed(struct ibv_pd *pd, void *addr,
+				   size_t length, enum ibv_access_flags access);
 int mlx4_rereg_mr(struct ibv_mr *mr, int flags, struct ibv_pd *pd,
 		  void *addr, size_t length, int access);
 int mlx4_dereg_mr(struct ibv_mr *mr);
+int mlx4_dereg_mr_relaxed(struct ibv_mr *mr);
+int mlx4_flush_relaxed_mr(struct ibv_pd *pd);
 
 struct ibv_mw *mlx4_alloc_mw(struct ibv_pd *pd, enum ibv_mw_type type);
 int mlx4_dealloc_mw(struct ibv_mw *mw);

providers/mlx4/verbs.c
@@ -330,6 +330,37 @@ struct ibv_mr *mlx4_reg_mr(struct ibv_pd *pd, void *addr, size_t length,
 	return mr;
 }
 
+struct ibv_mr *mlx4_reg_mr_relaxed(struct ibv_pd *pd, void *addr, size_t length,
+			   enum ibv_access_flags access)
+{
+	struct ibv_mr *mr;
+	struct ibv_reg_mr cmd;
+	int ret;
+
+	mr = malloc(sizeof *mr);
+	if (!mr)
+		return NULL;
+
+#ifdef IBV_CMD_REG_MR_RELAXED_HAS_RESP_PARAMS
+	{
+		struct ib_uverbs_reg_mr_resp resp;
+
+		ret = ibv_cmd_reg_mr_relaxed(pd, addr, length, (uintptr_t) addr,
+				     access, mr, &cmd, sizeof cmd,
+				     &resp, sizeof resp);
+	}
+#else
+	ret = ibv_cmd_reg_mr_relaxed(pd, addr, length, (uintptr_t) addr, access, mr,
+			     &cmd, sizeof cmd);
+#endif
+	if (ret) {
+		free(mr);
+		return NULL;
+	}
+
+	return mr;
+}
+
 int mlx4_rereg_mr(struct ibv_mr *mr,
 		  int flags,
 		  struct ibv_pd *pd, void *addr,
@@ -357,6 +388,29 @@ int mlx4_dereg_mr(struct ibv_mr *mr)
 		return ret;
 
 	free(mr);
+	return 0;
+}
+
+int mlx4_dereg_mr_relaxed(struct ibv_mr *mr)
+{
+	int ret;
+
+	ret = ibv_cmd_dereg_mr_relaxed(mr);
+	if (ret)
+		return ret;
+
+	free(mr);
+	return 0;
+}
+
+int mlx4_flush_relaxed_mr(struct ibv_pd *pd)
+{
+	int ret;
+
+	ret = ibv_cmd_flush_relaxed_mr(pd);
+	if (ret)
+		return ret;
+
 	return 0;
 }
 

providers/mlx4/mlx4.c
@@ -93,8 +93,11 @@ static const struct verbs_context_ops mlx4_ctx_ops = {
 	.share_pd      = mlx4_share_pd,
 #endif /* !WITHOUT_ORACLE_EXTENSIONS */
 	.reg_mr	       = mlx4_reg_mr,
+	.reg_mr_relaxed = mlx4_reg_mr_relaxed,
 	.rereg_mr      = mlx4_rereg_mr,
 	.dereg_mr      = mlx4_dereg_mr,
+	.dereg_mr_relaxed = mlx4_dereg_mr_relaxed,
+	.flush_relaxed_mr = mlx4_flush_relaxed_mr,
 	.alloc_mw      = mlx4_alloc_mw,
 	.dealloc_mw    = mlx4_dealloc_mw,
 	.bind_mw       = mlx4_bind_mw,

commit 69d42fffc2e653f7fd3a5294a321bf063f7d4865

libmlx4: Fix an incompatible pointer type compiler warning
This "initialization from incompatible pointer type" warning is a result
of the signature of mlx4_reg_mr_relaxed() differing from what is
expected by the mlx4_cx_ops structure in mlx4.c which is of type struct
verbs_context_ops. The .req_mr_relaxed field of the mlx4_cx_ops
structure is defined as:

struct ibv_mr *(*reg_mr_relaxed)(struct ibv_pd *pd, void *addr,
    size_t length, int access)

It seems best to change the mlx4_req_mr_relaxed() signature, rather
than the definition of the mlx4_cx_ops structure because precedence
already exists for defining the access as type 'int' rather than 'enum'
in the other prototype fields in the mlx4_cx_ops structure.

Fixes: 0bb394b0472f ("libmlx4: Add user level FMR support")

Orabug: 28769845

Signed-off-by: Mark Haywood <mark.haywood@oracle.com>
Reviewed-by: Gerd Rausch <gerd.rausch@oracle.com>
Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>
Reviewed-by: Yuval Shaia <yuval.shaia@oracle.com>
Signed-off-by: Aron Silverton <aron.silverton@oracle.com>

providers/mlx4/mlx4.h
@@ -343,7 +343,7 @@ struct ibv_mr *mlx4_reg_mr(struct ibv_pd *pd, void *addr,
 			    size_t length, int access);
 #ifndef WITHOUT_ORACLE_EXTENSIONS
 struct ibv_mr *mlx4_reg_mr_relaxed(struct ibv_pd *pd, void *addr,
-				   size_t length, enum ibv_access_flags access);
+				   size_t length, int access); 
 #endif
 int mlx4_rereg_mr(struct ibv_mr *mr, int flags, struct ibv_pd *pd,
 		  void *addr, size_t length, int access);

providers/mlx4/verbs.c
@@ -333,7 +333,7 @@ struct ibv_mr *mlx4_reg_mr(struct ibv_pd *pd, void *addr, size_t length,
 #ifndef WITHOUT_ORACLE_EXTENSIONS
 
 struct ibv_mr *mlx4_reg_mr_relaxed(struct ibv_pd *pd, void *addr, size_t length,
-			   enum ibv_access_flags access)
+			   int access)
 {
 	struct ibv_mr *mr;
 	struct ibv_reg_mr cmd;