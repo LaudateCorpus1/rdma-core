LaudateCorpus1/rdma-core

commit 8d16793fe8477f6742494e9d94e1fdd208ef12d4

libibverbs: Add Shared PD support
Orabug: 21525110

Introduce Shared PD Support in libibverbs.

Signed-off-by: Mukesh Kacker <mukesh.kacker@oracle.com>

Orabug: 28036886

Rebase to RDMA Core v17.1.

(cherry picked from commit 638d33a0b57538d56c76d8fed95dbf3609ab48ad)
(cherry picked from commit d5e5db8ef169879d4e873f6dabbd6ba2d3f3558f)
cherry-pick-repo=linux-git.us.oracle.com/RDMA/libibverbs.git

The two commits above were squashed. The first adds Shared PD support
to libibverbs and the second adds the man pages. There is no reason to
maintain these as separate commits.

Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
Reviewed-by: Shamir Rabinovitch <shamir.rabinovitch@oracle.com>
Acked-by: Aron Silverton <aron.silverton@oracle.com>

Orabug: 29356186

Rebase from RDMA Core 17.1 -> 18.2 -> 19.2

(cherry picked from commit 384399dfbfef9b85b0a149c695c4ce3cc0ae5b46)
(cherry picked from commit 657f73e6b1a625c606c1295746e436979a65509c)
(cherry picked from commit e90b65cd0ffd83c3f2648b51ccc2903790eb7a0f)
(cherry picked from commit fb55714d5728f3e88bb8ad87292ea8f01a97948f)
(cherry picked from commit 854e3206c0669aa0a28c193692b7bd0bd20a8d32)
cherry-pick-repo=linux-git.us.oracle.com/RDMA/rdma-core.git

The five commits above were squashed. The first commit adds Shared PD
support to libibverbs. The second commit adds an example program. The
third and fourth commits fix bugs in the example program added in the
second commit (bugs 26440016 and 28802228). The fifth commit adds
"#ifndef WITHOUT_ORACLE_EXTENSIONS" markers around Oracle extensions and
the relevant hunks for Shared PD are incorporated here.

Conflicts (17.1 -> 18.2):
        libibverbs/kern-abi.h

Upstream introduced the new DECLARE_CMD macro to use when defining verb
prototypes. That macro is used to define prototypes for ibv_alloc_shpd()
and ibv_share_pd().

Signed-off-by: Mark Haywood <mark.haywood@oracle.com>
Reviewed-by: Shamir Rabinovitch <shamir.rabinovitch@oracle.com>

kernel-headers/rdma/ib_user_verbs.h
@@ -88,6 +88,25 @@ enum {
 	IB_USER_VERBS_CMD_CLOSE_XRCD,
 	IB_USER_VERBS_CMD_CREATE_XSRQ,
 	IB_USER_VERBS_CMD_OPEN_QP,
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	/*
+	 * Note: 0-40 verbs defined above
+	 * Start oracle verb additions leaving a gap
+	 * for upstream verbs growth.
+	 *
+	 * We start at 46 which is the starting value used
+	 * for these verbs in UEK2 and add them in same
+	 * order.
+	 *
+	 * (Even if we dont care about aligning with UEK2 values,
+	 *  cannot go beyond 63 because of "struct ib_device"
+	 *  has uverbs_cmd_mask which is 64 bits wide!)
+	 */
+#define IB_USER_VERBS_CMD_ORACLE_ADDS_START 46
+	IB_USER_VERBS_CMD_ALLOC_SHPD = IB_USER_VERBS_CMD_ORACLE_ADDS_START,
+					/* =46 */
+	IB_USER_VERBS_CMD_SHARE_PD,	/* =47 */
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 };
 
 enum {
@@ -315,6 +334,34 @@ struct ib_uverbs_alloc_pd_resp {
 struct ib_uverbs_dealloc_pd {
 	__u32 pd_handle;
 };
+
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+
+struct ib_uverbs_alloc_shpd {
+	__u64 response;
+	__u32 pd_handle;
+	__u32 reserved;
+	__u64 share_key;
+	__u64 driver_data[0];
+};
+
+struct ib_uverbs_alloc_shpd_resp {
+	__u32 shpd_handle;
+};
+
+struct ib_uverbs_share_pd {
+	__u64 response;
+	__u32 shpd_handle;
+	__u32 reserved;
+	__u64 share_key;
+	__u64 driver_data[0];
+};
+
+struct ib_uverbs_share_pd_resp {
+	__u32 pd_handle;
+};
+
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 
 struct ib_uverbs_open_xrcd {
 	__aligned_u64 response;

libibverbs/cmd.c
@@ -350,6 +350,51 @@ int ibv_cmd_dealloc_pd(struct ibv_pd *pd)
 	return 0;
 }
 
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+
+int ibv_cmd_alloc_shpd(struct ibv_context *context, struct ibv_pd *pd,
+		       uint64_t share_key, struct ibv_shpd *shpd,
+		       struct ibv_alloc_shpd *cmd, size_t cmd_size,
+		       struct ib_uverbs_alloc_shpd_resp *resp, size_t resp_size)
+{
+	IBV_INIT_CMD_RESP(cmd, cmd_size, ALLOC_SHPD, resp, resp_size);
+
+	cmd->pd_handle = pd->handle;
+	cmd->share_key = share_key;
+
+	if (write(context->cmd_fd, cmd, cmd_size) != cmd_size)
+		return errno;
+
+	(void) VALGRIND_MAKE_MEM_DEFINED(resp, resp_size);
+
+	shpd->handle  = resp->shpd_handle;
+
+	return 0;
+}
+
+int ibv_cmd_share_pd(struct ibv_context *context, struct ibv_shpd *shpd,
+		     uint64_t share_key, struct ibv_pd *pd,
+		     struct ibv_share_pd *cmd, size_t cmd_size,
+		     struct ib_uverbs_share_pd_resp *resp, size_t resp_size)
+{
+	IBV_INIT_CMD_RESP(cmd, cmd_size, SHARE_PD, resp, resp_size);
+
+	cmd->shpd_handle = shpd->handle;
+	cmd->share_key = share_key;
+
+	if (write(context->cmd_fd, cmd, cmd_size) != cmd_size)
+		return errno;
+
+	(void) VALGRIND_MAKE_MEM_DEFINED(resp, resp_size);
+
+	pd->handle  = resp->pd_handle;
+	pd->context = context;
+
+	return 0;
+}
+
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
+
 int ibv_cmd_open_xrcd(struct ibv_context *context, struct verbs_xrcd *xrcd,
 		      int vxrcd_size,
 		      struct ibv_xrcd_init_attr *attr,

libibverbs/driver.h
@@ -224,6 +224,9 @@ struct verbs_context_ops {
 		struct ibv_context *context,
 		struct ibv_parent_domain_init_attr *attr);
 	struct ibv_pd *(*alloc_pd)(struct ibv_context *context);
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	struct ibv_shpd *(*alloc_shpd)(struct ibv_pd *pd, uint64_t share_key, struct ibv_shpd *shpd);
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 	struct ibv_td *(*alloc_td)(struct ibv_context *context,
 				   struct ibv_td_init_attr *init_attr);
 	void (*async_event)(struct ibv_async_event *event);
@@ -332,6 +335,9 @@ struct verbs_context_ops {
 	int (*rereg_mr)(struct verbs_mr *vmr, int flags, struct ibv_pd *pd,
 			void *addr, size_t length, int access);
 	int (*resize_cq)(struct ibv_cq *cq, int cqe);
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	struct ibv_pd *(*share_pd)(struct ibv_context *context, struct ibv_shpd *shpd, uint64_t share_key);
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 };
 
 static inline struct verbs_device *
@@ -414,6 +420,19 @@ int ibv_cmd_alloc_pd(struct ibv_context *context, struct ibv_pd *pd,
 		     struct ibv_alloc_pd *cmd, size_t cmd_size,
 		     struct ib_uverbs_alloc_pd_resp *resp, size_t resp_size);
 int ibv_cmd_dealloc_pd(struct ibv_pd *pd);
+
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+int ibv_cmd_alloc_shpd(struct ibv_context *context, struct ibv_pd *pd,
+		       uint64_t share_key, struct ibv_shpd *shpd,
+		       struct ibv_alloc_shpd *cmd, size_t cmd_size,
+		       struct ib_uverbs_alloc_shpd_resp *resp,
+		       size_t resp_size);
+int ibv_cmd_share_pd(struct ibv_context *context, struct ibv_shpd *shpd,
+		     uint64_t share_key, struct ibv_pd *pd,
+		     struct ibv_share_pd *cmd, size_t cmd_size,
+		     struct ib_uverbs_share_pd_resp *resp, size_t resp_size);
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
+
 int ibv_cmd_open_xrcd(struct ibv_context *context, struct verbs_xrcd *xrcd,
 		      int vxrcd_size,
 		      struct ibv_xrcd_init_attr *attr,

libibverbs/dummy_ops.c
@@ -66,6 +66,24 @@ static struct ibv_pd *alloc_pd(struct ibv_context *context)
 	return NULL;
 }
 
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+
+static struct ibv_shpd *
+alloc_shpd(struct ibv_pd *pd, uint64_t share_key, struct ibv_shpd *shpd)
+{
+	errno = ENOSYS;
+	return NULL;
+}
+
+static struct ibv_pd *
+share_pd(struct ibv_context *context, struct ibv_shpd *shpd, uint64_t share_key)
+{
+	errno = ENOSYS;
+	return NULL;
+}
+
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
+
 static struct ibv_td *alloc_td(struct ibv_context *context,
 			       struct ibv_td_init_attr *init_attr)
 {
@@ -441,6 +459,9 @@ const struct verbs_context_ops verbs_dummy_ops = {
 	alloc_null_mr,
 	alloc_parent_domain,
 	alloc_pd,
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	alloc_shpd,
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 	alloc_td,
 	async_event,
 	attach_counters_point_flow,
@@ -501,6 +522,9 @@ const struct verbs_context_ops verbs_dummy_ops = {
 	req_notify_cq,
 	rereg_mr,
 	resize_cq,
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	share_pd,
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 };
 
 /*
@@ -554,6 +578,9 @@ void verbs_set_ops(struct verbs_context *vctx,
 	SET_OP(ctx, alloc_mw);
 	SET_OP(vctx, alloc_null_mr);
 	SET_PRIV_OP(ctx, alloc_pd);
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	SET_PRIV_OP_IC(ctx, alloc_shpd);
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 	SET_OP(vctx, alloc_parent_domain);
 	SET_OP(vctx, alloc_td);
 	SET_OP(vctx, attach_counters_point_flow);
@@ -615,6 +642,9 @@ void verbs_set_ops(struct verbs_context *vctx,
 	SET_OP(ctx, req_notify_cq);
 	SET_PRIV_OP(ctx, rereg_mr);
 	SET_PRIV_OP(ctx, resize_cq);
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	SET_PRIV_OP_IC(ctx, share_pd);
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 
 #undef SET_OP
 #undef SET_OP2

libibverbs/examples/CMakeLists.txt
@@ -26,3 +26,6 @@ target_link_libraries(ibv_ud_pingpong LINK_PRIVATE ibverbs ibverbs_tools)
 
 rdma_executable(ibv_xsrq_pingpong xsrq_pingpong.c)
 target_link_libraries(ibv_xsrq_pingpong LINK_PRIVATE ibverbs ibverbs_tools)
+
+rdma_executable(ibv_shpd_pingpong shpd_pingpong.c)
+target_link_libraries(ibv_shpd_pingpong LINK_PRIVATE ibverbs ibverbs_tools)

libibverbs/kern-abi.h
@@ -174,6 +174,10 @@ struct empty {};
 
 DECLARE_CMD(IB_USER_VERBS_CMD_ALLOC_MW, ibv_alloc_mw, ib_uverbs_alloc_mw);
 DECLARE_CMD(IB_USER_VERBS_CMD_ALLOC_PD, ibv_alloc_pd, ib_uverbs_alloc_pd);
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+DECLARE_CMD(IB_USER_VERBS_CMD_ALLOC_SHPD, ibv_alloc_shpd, ib_uverbs_alloc_shpd);
+DECLARE_CMD(IB_USER_VERBS_CMD_SHARE_PD, ibv_share_pd, ib_uverbs_share_pd);
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 DECLARE_CMDX(IB_USER_VERBS_CMD_ATTACH_MCAST, ibv_attach_mcast, ib_uverbs_attach_mcast, empty);
 DECLARE_CMDX(IB_USER_VERBS_CMD_CLOSE_XRCD, ibv_close_xrcd, ib_uverbs_close_xrcd, empty);
 DECLARE_CMD(IB_USER_VERBS_CMD_CREATE_AH, ibv_create_ah, ib_uverbs_create_ah);

libibverbs/libibverbs.map.in
@@ -56,6 +56,8 @@ IBVERBS_1.1 {
 		ibv_ack_async_event;
 		ibv_ack_cq_events;
 		ibv_alloc_pd;
+		ibv_alloc_shpd;
+		ibv_share_pd;
 		ibv_attach_mcast;
 		ibv_close_device;
 		ibv_create_ah;
@@ -123,6 +125,8 @@ IBVERBS_PRIVATE_@IBVERBS_PABI_VERSION@ {
 		ibv_cmd_alloc_dm;
 		ibv_cmd_alloc_mw;
 		ibv_cmd_alloc_pd;
+		ibv_cmd_alloc_shpd;
+		ibv_cmd_share_pd;
 		ibv_cmd_attach_mcast;
 		ibv_cmd_close_xrcd;
 		ibv_cmd_create_ah;

commit c363a52aac510f40e3479aa10dd9bfbd9babe128

libibverbs: convert SHPD struct fields from __u64 to __aligned_64
As explained in linux/types.h:
__aligned_u64 should be used in defining kernel<->userspace ABIs to
avoid common 32/64-bit compat problems.

Orabug: 29460706

Fixes: 8d16793fe847 ("libibverbs: Add Shared PD support")

Signed-off-by: Mark Haywood <mark.haywood@oracle.com>
Reviewed-by: Shamir Rabinovitch <shamir.rabinovitch@oracle.com>
Acked-by: Aron Silverton <aron.silverton@oracle.com>

kernel-headers/rdma/ib_user_verbs.h
@@ -138,7 +138,7 @@ enum {
  * that they pack the same way on 32-bit and 64-bit architectures (to
  * avoid incompatibility between 32-bit userspace and 64-bit kernels).
  * Specifically:
- *  - Do not use pointer types -- pass pointers in __u64 instead.
+ *  - Do not use pointer types -- pass pointers in __aligned_64 instead.
  *  - Make sure that any structure larger than 4 bytes is padded to a
  *    multiple of 8 bytes.  Otherwise the structure size will be
  *    different between 32-bit and 64-bit architectures.
@@ -347,11 +347,11 @@ struct ib_uverbs_dealloc_pd {
 #ifndef WITHOUT_ORACLE_EXTENSIONS
 
 struct ib_uverbs_alloc_shpd {
-	__u64 response;
+	__aligned_u64 response;
 	__u32 pd_handle;
 	__u32 reserved;
-	__u64 share_key;
-	__u64 driver_data[0];
+	__aligned_u64 share_key;
+	__aligned_u64 driver_data[0];
 };
 
 struct ib_uverbs_alloc_shpd_resp {
@@ -359,11 +359,11 @@ struct ib_uverbs_alloc_shpd_resp {
 };
 
 struct ib_uverbs_share_pd {
-	__u64 response;
+	__aligned_u64 response;
 	__u32 shpd_handle;
 	__u32 reserved;
-	__u64 share_key;
-	__u64 driver_data[0];
+	__aligned_u64 share_key;
+	__aligned_u64 driver_data[0];
 };
 
 struct ib_uverbs_share_pd_resp {