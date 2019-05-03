LaudateCorpus1/rdma-core

commit 2267874b3889e78588de6ef779e4b89f4e4dfb28

libmlx4: XRC binary compat layer
Orabug: 21616281

(Ported from Mellanox OFED 2.4 libmlx4)

Signed-off-by: Yishai Hadas <yishaih@mellanox.com>
Signed-off-by: Mukesh Kacker <mukesh.kacker@oracle.com>

Orabug: 28037496

Rebase to RDMA Core v17.1.

(cherry picked from commit f97d263c4084e45af856831ac642d4fff78624e4)
(cherry picked from commit 87a260f612bf08d23f6005ef28dc704468bceaca)
(cherry picked from commit bcde3ec3560465e0bbb629d58adecd6efd1cf89e)
(cherry picked from commit 70017dd4f89c89f977bff8182c0de26f601376d5)
cherry-pick-repo=linux-git.us.oracle.com/RDMA/libmlx4.git

Signed-off-by: Mark Haywood <mark.haywood@oracle.com>

Conflicts:
	providers/mlx4/mlx4.c
	providers/mlx4/mlx4.h

The commits above were squashed. The first commit introduced the feature
and the next three commits fixed issues related to the first. There is
no reason to maintain them as separate commits.

Signed-off-by: Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
Reviewed-by: Gerd Rausch <gerd.rausch@oracle.com>
Reviewed-by: Yuval Shaia <yuval.shaia@oracle.com>
Acked-by: Aron Silverton <aron.silverton@oracle.com>

Orabug: 29356186

Rebase from RDMA Core 17.1 -> 18.2 -> 19.2.

(cherry picked from commit 7312f923bda2265ac85343b760aabf6ba2a85868)
(cherry picked from commit 24b30bf441443f9420faa484a6b354103c8e61e6)
(cherry picked from commit d427ebbe439c53c97e344edb8f80addf0f521e03)
(cherry picked from commit 854e3206c0669aa0a28c193692b7bd0bd20a8d32)
cherry-pick-repo=linux-git.us.oracle.com/RDMA/rdma-core.git

The four commits above were squashed. The first commit adds the binary
XRC compatibility layer for the mlx4 verbs provider. The second commit
adds a code dependency without backporting additional unneeded upstream
commits. The third commit fixes a potential bug in the first commit (bug
21616281). The fourth commit adds "#ifndef WITHOUT_ORACLE_EXTENSIONS"
markers around Oracle extensions and the relevant hunks for mlx4 XRC
binary compatibility were incorporated here.

providers/mlx4/mlx4.h
@@ -185,6 +185,10 @@ struct mlx4_srq {
 	__be32			       *db;
 	uint16_t			counter;
 	uint8_t				ext_srq;
+
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	struct ibv_srq_legacy *ibv_srq_legacy;
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 };
 
 struct mlx4_wq {
@@ -423,5 +427,12 @@ int mlx4_post_wq_recv(struct ibv_wq *ibwq, struct ibv_recv_wr *wr,
 		      struct ibv_recv_wr **bad_wr);
 struct ibv_flow *mlx4_create_flow(struct ibv_qp *qp, struct ibv_flow_attr *flow_attr);
 int mlx4_destroy_flow(struct ibv_flow *flow_id);
+
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+void *mlx4_get_legacy_xrc(struct ibv_srq *srq);
+void mlx4_set_legacy_xrc(struct ibv_srq *srq, void *legacy_xrc_srq);
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
+
+extern int mlx4_trace;
 
 #endif /* MLX4_H */

providers/mlx4/mlx4.c
@@ -47,6 +47,10 @@
 #define PCI_VENDOR_ID_MELLANOX			0x15b3
 #endif
 
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+int mlx4_trace = 0;
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
+
 #define HCA(v, d) VERBS_PCI_MATCH(PCI_VENDOR_ID_##v, d, NULL)
 static const struct verbs_match_ent hca_table[] = {
 	HCA(MELLANOX, 0x6340),	/* MT25408 "Hermon" SDR */
@@ -134,7 +138,22 @@ static const struct verbs_context_ops mlx4_ctx_ops = {
 	.open_xrcd = mlx4_open_xrcd,
 	.query_device_ex = mlx4_query_device_ex,
 	.query_rt_values = mlx4_query_rt_values,
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	.drv_set_legacy_xrc = mlx4_set_legacy_xrc,
+	.drv_get_legacy_xrc = mlx4_get_legacy_xrc,
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 };
+
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+static void mlx4_read_env(void)
+{
+	char *env_value;
+
+	env_value = getenv("MLX4_TRACE");
+	if (env_value && (strcmp(env_value, "0")))
+		mlx4_trace = 1;
+}
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 
 static int mlx4_map_internal_clock(struct mlx4_device *mdev,
 				   struct ibv_context *ibv_ctx)
@@ -177,6 +196,10 @@ static struct verbs_context *mlx4_alloc_context(struct ibv_device *ibdev,
 		return NULL;
 
 	verbs_ctx = &context->ibv_ctx;
+
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	mlx4_read_env();
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 
 	if (dev->abi_version <= MLX4_UVERBS_NO_DEV_CAPS_ABI_VERSION) {
 		if (ibv_cmd_get_context(verbs_ctx, &cmd, sizeof(cmd),

providers/mlx4/qp.c
@@ -270,6 +270,9 @@ int mlx4_post_send(struct ibv_qp *ibqp, struct ibv_send_wr *wr,
 
 		switch (ibqp->qp_type) {
 		case IBV_QPT_XRC_SEND:
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+		case IBV_QPT_XRC:
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 			ctrl->srcrb_flags |= MLX4_REMOTE_SRQN_FLAGS(wr);
 			/* fall through */
 		case IBV_QPT_RC:
@@ -649,6 +652,9 @@ void mlx4_calc_sq_wqe_size(struct ibv_qp_cap *cap, enum ibv_qp_type type,
 		break;
 
 	case IBV_QPT_XRC_SEND:
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	case IBV_QPT_XRC:
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 	case IBV_QPT_RC:
 		size += sizeof (struct mlx4_wqe_raddr_seg);
 		/*
@@ -754,6 +760,9 @@ void mlx4_set_sq_sizes(struct mlx4_qp *qp, struct ibv_qp_cap *cap,
 		break;
 
 	case IBV_QPT_XRC_SEND:
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	case IBV_QPT_XRC:
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 	case IBV_QPT_UC:
 	case IBV_QPT_RC:
 		wqe_size -= sizeof (struct mlx4_wqe_raddr_seg);

providers/mlx4/srq.c
@@ -61,15 +61,24 @@ int mlx4_post_srq_recv(struct ibv_srq *ibsrq,
 		       struct ibv_recv_wr *wr,
 		       struct ibv_recv_wr **bad_wr)
 {
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	struct mlx4_srq *srq;
+#else
 	struct mlx4_srq *srq = to_msrq(ibsrq);
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 	struct mlx4_wqe_srq_next_seg *next;
 	struct mlx4_wqe_data_seg *scat;
 	int err = 0;
 	int nreq;
 	int i;
 
-	pthread_spin_lock(&srq->lock);
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	if (ibsrq->handle == LEGACY_XRC_SRQ_HANDLE)
+		ibsrq = (struct ibv_srq *)(((struct ibv_srq_legacy *) ibsrq)->ibv_srq);
+	srq = to_msrq(ibsrq);
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 
+	pthread_spin_lock(&srq->lock);
 	for (nreq = 0; wr; ++nreq, wr = wr->next) {
 		if (wr->num_sge > srq->max_gs) {
 			err = -1;

providers/mlx4/verbs.c
@@ -696,6 +696,24 @@ int mlx4_destroy_cq(struct ibv_cq *cq)
 	return 0;
 }
 
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+
+void *mlx4_get_legacy_xrc(struct ibv_srq *srq)
+{
+	struct mlx4_srq	*msrq = to_msrq(srq);
+
+	return msrq->ibv_srq_legacy;
+}
+
+void mlx4_set_legacy_xrc(struct ibv_srq *srq, void *legacy_xrc_srq)
+{
+	struct mlx4_srq	*msrq = to_msrq(srq);
+
+	msrq->ibv_srq_legacy = legacy_xrc_srq;
+}
+
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
+
 struct ibv_srq *mlx4_create_srq(struct ibv_pd *pd,
 				struct ibv_srq_init_attr *attr)
 {
@@ -708,7 +726,11 @@ struct ibv_srq *mlx4_create_srq(struct ibv_pd *pd,
 	if (attr->attr.max_wr > 1 << 16 || attr->attr.max_sge > 64)
 		return NULL;
 
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	srq = calloc(1, sizeof *srq);
+#else
 	srq = malloc(sizeof *srq);
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 	if (!srq)
 		return NULL;
 
@@ -771,6 +793,11 @@ int mlx4_modify_srq(struct ibv_srq *srq,
 {
 	struct ibv_modify_srq cmd;
 
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	if (srq->handle == LEGACY_XRC_SRQ_HANDLE)
+		srq = (struct ibv_srq *)(((struct ibv_srq_legacy *) srq)->ibv_srq);
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
+
 	return ibv_cmd_modify_srq(srq, attr, attr_mask, &cmd, sizeof cmd);
 }
 
@@ -779,15 +806,38 @@ int mlx4_query_srq(struct ibv_srq *srq,
 {
 	struct ibv_query_srq cmd;
 
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	if (srq->handle == LEGACY_XRC_SRQ_HANDLE)
+		srq = (struct ibv_srq *)(((struct ibv_srq_legacy *) srq)->ibv_srq);
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
+
 	return ibv_cmd_query_srq(srq, attr, &cmd, sizeof cmd);
 }
 
 int mlx4_destroy_srq(struct ibv_srq *srq)
 {
 	int ret;
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	struct ibv_srq *legacy_srq = NULL;
 
-	if (to_msrq(srq)->ext_srq)
-		return mlx4_destroy_xrc_srq(srq);
+	if (srq->handle == LEGACY_XRC_SRQ_HANDLE) {
+		legacy_srq = srq;
+		srq = (struct ibv_srq *)(((struct ibv_srq_legacy *) srq)->ibv_srq);
+	}
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
+
+	if (to_msrq(srq)->ext_srq) {
+		ret =  mlx4_destroy_xrc_srq(srq);
+		if (ret)
+			return ret;
+
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+		if (legacy_srq)
+			free(legacy_srq);
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
+
+		return 0;
+	}
 
 	ret = ibv_cmd_destroy_srq(srq);
 	if (ret)
@@ -934,6 +984,12 @@ static struct ibv_qp *create_qp_ex(struct ibv_context *context,
 	if (attr->comp_mask & ~MLX4_CREATE_QP_SUP_COMP_MASK)
 		return NULL;
 
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	if (attr->qp_type == IBV_QPT_XRC && attr->recv_cq &&
+		attr->cap.max_recv_wr > 0 && mlx4_trace)
+		fprintf(stderr, PFX "Warning: Legacy XRC sender should not use a recieve cq\n");
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
+
 	qp = calloc(1, sizeof *qp);
 	if (!qp)
 		return NULL;
@@ -951,7 +1007,11 @@ static struct ibv_qp *create_qp_ex(struct ibv_context *context,
 	}
 
 	if (attr->srq || attr->qp_type == IBV_QPT_XRC_SEND ||
-	    attr->qp_type == IBV_QPT_XRC_RECV) {
+	    attr->qp_type == IBV_QPT_XRC_RECV
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	    || attr->qp_type == IBV_QPT_XRC
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
+	    ) {
 		attr->cap.max_recv_wr = qp->rq.wqe_cnt = attr->cap.max_recv_sge = 0;
 	} else {
 		qp->rq.wqe_cnt = align_queue_size(attr->cap.max_recv_wr);
@@ -1072,12 +1132,34 @@ struct ibv_qp *mlx4_create_qp(struct ibv_pd *pd, struct ibv_qp_init_attr *attr)
 	struct ibv_qp_init_attr_ex attr_ex;
 	struct ibv_qp *qp;
 
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	/*
+	 * We should copy below only the shared fields excluding the xrc_domain
+	 * field. Otherwise we may have an ABI issue with applications that
+	 * were compiled without the xrc_domain field. The xrc_domain any way
+	 * has no affect in the sender side, no need to copy in/out.
+	 */
+	int init_attr_base_size = offsetof(struct ibv_qp_init_attr,
+		xrc_domain);
+
+	memset(&attr_ex, 0, sizeof(attr_ex)); /* pre-set all fields to zero */
+	/* copying only shared fields */
+	memcpy(&attr_ex, attr, init_attr_base_size);
+#else /* WITHOUT_ORACLE_EXTENSIONS */
 	memcpy(&attr_ex, attr, sizeof *attr);
+#endif /* WITHOUT_ORACLE_EXTENSIONS */
+
 	attr_ex.comp_mask = IBV_QP_INIT_ATTR_PD;
 	attr_ex.pd = pd;
 	qp = mlx4_create_qp_ex(pd->context, &attr_ex);
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	if (qp)
+		memcpy(attr, &attr_ex, init_attr_base_size);
+#else /* WITHOUT_ORACLE_EXTENSIONS */
 	if (qp)
 		memcpy(attr, &attr_ex, sizeof *attr);
+#endif /* WITHOUT_ORACLE_EXTENSIONS */
+
 	return qp;
 }
 

commit fd8e4130c8f60179735956df4fe8db7ffe22a4ca

libmlx4: extern of mlx4_trace should be Oracle only
The mlx4_trace variable is specific to the Oracle implemetation.

Orabug: 29480888

Fixes: 2267874b3889 ("libmlx4: XRC binary compat layer")

Signed-off-by: Mark Haywood <mark.haywood@oracle.com>
Reviewed-by: Qing Huang <qing.huang@oracle.com>
Acked-by: Aron Silverton <aron.silverton@oracle.com>

providers/mlx4/mlx4.h
@@ -442,8 +442,8 @@ int mlx4_destroy_flow(struct ibv_flow *flow_id);
 #ifndef WITHOUT_ORACLE_EXTENSIONS
 void *mlx4_get_legacy_xrc(struct ibv_srq *srq);
 void mlx4_set_legacy_xrc(struct ibv_srq *srq, void *legacy_xrc_srq);
-#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 
 extern int mlx4_trace;
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 
 #endif /* MLX4_H */